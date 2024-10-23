522ae5d196d13ec7d0ade7f9d3f849d306b9584c
pesati <pesatirevoredo@gmail.com>
2024-10-22 21:30:49 -0300

    add exercicios

---
+++ exercicios.py
@@ -0,0 +1,37 @@
+import csv
+
+# Caminho para o arquivo CSV
+caminho_arquivo = 'exemplo.csv'
+
+# Inicializa uma lista vazia para armazenar os dados
+dados = []
+
+# Usa o gerenciador de contexto `with` para abrir o arquivo
+with open(caminho_arquivo, mode='r', encoding='utf-8') as arquivo:
+    # Cria um objeto leitor de CSV
+    leitor_csv = csv.DictReader(arquivo)
+    
+    # Itera sobre as linhas do arquivo CSV
+    for linha in leitor_csv:
+        # Adiciona cada linha (um dicionário) à lista de dados
+        dados.append(linha)
+
+# Exibe os dados lidos do arquivo CSV
+for registro in dados:
+    print(registro)
+    
+####
+
+lista = [64, 34, 25, 12, 22, 11, 90]
+
+def ordernar_lista(lista: list) -> list:
+    lista_ordenada = lista.copy()
+
+    for i in range(len(lista)):
+        for j in range(i+1, len(lista)):
+            if lista[i] > lista[j]:
+                lista[i], lista[j] = lista[j], lista[i]
+
+    return lista_ordenada
+
+print(lista)
